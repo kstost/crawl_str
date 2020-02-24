a5076d2a45fdcd3e7f97051b0fb625362f04b3a3
KST <kstost@gmail.com>
2020-02-25 03:58:33 +0900

    fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require('fs');
 const md5 = require('md5');
-module.exports = function (str, complete, contain_dir) {
+module.exports = function (str, cbs, contain_dir) {
     let counter = 0;
     let list = str.trim().split('\n').map(s => s.trim()).filter(a => (a.length && a.indexOf('http') === 0));
     list = [...new Set(list)];
@@ -11,21 +11,17 @@ module.exports = function (str, complete, contain_dir) {
     function getUrl(url) {
         let file = contain_dir + md5(url) + '.txt';
         (async () => {
+            let fails;
+            let crs, skip = false;
             try {
                 let stats2 = await new Promise((oo, xx) => {
-                    fs.lstat(contain_dir, function (e, stats) {
-                        oo(stats);
-                    });
+                    fs.lstat(contain_dir, function (e, stats) { oo(stats); });
                 });
                 if (!stats2) {
-                    await new Promise((oo, xx) => {
-                        fs.mkdir(contain_dir, a => { oo(); });
-                    });
+                    await new Promise((oo, xx) => { fs.mkdir(contain_dir, a => { oo(); }); });
                 }
                 let rs = await new Promise((oo, xx) => {
-                    fs.lstat(file, function (e, stats) {
-                        oo(!(stats && stats.isFile()));
-                    });
+                    fs.lstat(file, function (e, stats) { oo(!(stats && stats.isFile())); });
                 });
                 if (rs) {
                     let html = await axios.get(url);
@@ -36,16 +32,26 @@ module.exports = function (str, complete, contain_dir) {
                         statusText: html.statusText,
                         status: html.status
                     }
-                    fs.writeFile(file, JSON.stringify(wo), d => {
-                        console.log('complete - ' + url);
+                    await new Promise((oo, xx) => {
+                        fs.writeFile(file, JSON.stringify(wo), d => {
+                            oo();
+                        });
                     });
+                } else {
+                    skip = true;
                 }
+                crs = { url: url, error: null, skip: skip };
             } catch (e) {
-                console.log('fail - ' + url);
+                crs = { url: url, error: e };
+                if (!fails) { fails = []; }
+                fails.push(crs);
             }
             counter++;
-            if (counter === list.length) {
-                complete();
+            if (cbs.progress) {
+                cbs.progress(crs, counter / list.length);
+            }
+            if (counter === list.length && cbs.complete) {
+                cbs.complete(fails);
             }
         })();
     }
