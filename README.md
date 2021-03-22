8c2be5338923e333ba10ab6f3a9d369dfbf23878
kstost <monogatree@gmail.com>
2021-03-23 06:04:56 +0900

    fixed

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# crawl_str
+# intall
+```
+$ npm install crawl_str
+```
+# usage
+```js
+const crawl_str = require('crawl_str');
+crawl_str(`
+Make url list here
+Lines that doesn't start with 'http' or 'https' are gonna be ignored
+
+
+https://archive.is/ALjBy
+https://archive.is/ALjBy
+https://archive.is/ALjBy
+https://archive.is/ALjBy
+https://archive.is/ALjBy
+
+
+`, {
+    progress: (info, progress_ratio) => {
+        console.log('progress', info.url, progress_ratio);
+    },
+    complete: faillist => {
+        console.log('complete');
+    }
+}, './data/', 'html');
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+const fs = require('fs');
+const md5 = require('md5');
+module.exports = function (str, cbs, contain_dir, ext) {
+    let counter = 0;
+    let list = str.trim().split('\n').map(s => s.trim()).filter(a => (a.length && a.indexOf('http') === 0));
+    list = [...new Set(list)];
+    list.forEach(url => {
+        getUrl(url);
+    });
+    function getUrl(url) {
+        let file = contain_dir + md5(url) + '.' + ext;
+        (async () => {
+            let fails;
+            let crs, skip = false;
+            try {
+                let stats2 = await new Promise((oo, xx) => {
+                    fs.lstat(contain_dir, function (e, stats) { oo(stats); });
+                });
+                if (!stats2) {
+                    await new Promise((oo, xx) => { fs.mkdir(contain_dir, a => { oo(); }); });
+                }
+                let rs = await new Promise((oo, xx) => {
+                    fs.lstat(file, function (e, stats) { oo(!(stats && stats.isFile())); });
+                });
+                if (rs) {
+                    let html = await axios.get(url);
+                    let wo = {
+                        url: url,
+                        data: html.data,
+                        headers: html.headers,
+                        statusText: html.statusText,
+                        status: html.status
+                    }
+                    await new Promise((oo, xx) => {
+                        fs.writeFile(file, JSON.stringify(wo), d => {
+                            oo();
+                        });
+                    });
+                } else {
+                    skip = true;
+                }
+                crs = { url: url, error: null, skip: skip };
+            } catch (e) {
+                crs = { url: url, error: e };
+                if (!fails) { fails = []; }
+                fails.push(crs);
+            }
+            counter++;
+            if (cbs.progress) {
+                cbs.progress(crs, counter / list.length);
+            }
+            if (counter === list.length && cbs.complete) {
+                cbs.complete(fails);
+            }
+        })();
+    }
+    return list;
+};
